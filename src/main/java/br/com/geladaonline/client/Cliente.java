80af29da6bb966da740776ee1be6c55814f929f7
Fábio <fabio.dev.gomes@gmail.com>
2017-02-07 14:21:17 -0200

    Implementação do cliente com requisição xml

--- src/main/java/br/com/geladaonline/client/Cliente.java
+++ src/main/java/br/com/geladaonline/client/Cliente.java
@@ -1,8 +1,14 @@
 package br.com.geladaonline.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
 
+import br.com.geladaonline.model.Cerveja;
 import br.com.geladaonline.model.rest.Cervejas;
 
 public class Cliente {
@@ -15,6 +21,16 @@ public class Cliente {
 		
 		System.out.println(cervejas);
 		System.out.println(cervejas.getLinks().size());
+		
+		List<Cerveja> cervejasList = new ArrayList<>();
+		
+		for (Link link : cervejas.getLinks()) {
+			Cerveja cerveja = ClientBuilder.newClient().invocation(link)
+					.accept(MediaType.APPLICATION_XML)
+					.get(Cerveja.class);
+			
+			cervejasList.add(cerveja);
+		}
 		
 	}
 	
